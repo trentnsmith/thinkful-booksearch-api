a83e02549d35cd37a62ba37cbddfec69b3243af5
Trent Smith <trentnsmith@gmail.com>
2020-04-04 21:15:12 -0500

    added components

---
+++ src/SearchInput.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class SearchInput extends Component {
+    render() {
+        return(
+            <div className="searchBar">
+                <form className="searchForm">
+                    <label htmlFor="bar" className="label">
+                        Search:
+                        <input type="text" placeholder="Enter search keyword" id="input"></input>
+                    </label>
+                    <button type="submit" className="search-button"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        this.props.handleSubmit()
+                    }}>
+                        Search
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchInput;
